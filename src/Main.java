f20492f890a39962392c69660e66736c65e46534
Bruno Kambere <brunokambere@gmail.com>
2024-01-30 23:08:07 +0200

    linkedlist usage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import datastructures.linkedlist.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // LinkedList
+        LinkedList linkedList = new LinkedList();
+        linkedList.addLast(10);
+        linkedList.addLast(20);
+        linkedList.addLast(30);
+        // linkedList : [10, 20, 30]
+
+        linkedList.addFirst(5);
+        // linkedList : [5, 10, 20, 30]
+
+        linkedList.removeFirst();
+        linkedList.contains(5); // returns false
     }
 }
