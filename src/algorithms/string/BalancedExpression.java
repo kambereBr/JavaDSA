58d7a40918509c8edf16ffd6fe26492c4635b9cb
Bruno Kambere <brunokambere@gmail.com>
2024-02-04 23:30:33 +0200

    refactor BalancedExpression class by handling other brackets

--- src/algorithms/string/BalancedExpression.java
+++ src/algorithms/string/BalancedExpression.java
@@ -1,21 +1,39 @@
 package algorithms.string;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class BalancedExpression {
+    private final List<Character> leftBrackets = Arrays.asList('(', '{', '<', '[');
+    private final List<Character> rightBrackets = Arrays.asList(')', '}', '>', ']');
     public boolean isBalanced(String str) {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : str.toCharArray()) {
-            if (ch == '(')
+            if (isLeftBracket(ch))
                 stack.push('(');
 
-            if (ch == ')') {
+            if (isRightBracket(ch)) {
                 if (stack.isEmpty()) return false;
-                stack.pop();
+
+                var top = stack.pop();
+                if (!bracketsMatch(top, ch)) return false;
             }
         }
 
         return stack.isEmpty();
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
